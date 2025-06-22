a8e31b8b348dccf7e74781c866b60dc64f702863
Sia Billal Quba <siabillal@gmail.com>
2025-06-22 22:41:23 +0700

    feat: get / return json message hello

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    message: "Hello",
+  });
+});
 
-export default app
+export default app;
