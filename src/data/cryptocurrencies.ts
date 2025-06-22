79ddee17166343b24c14489d2efcc50986de5770
Sia Billal Quba <siabillal@gmail.com>
2025-06-22 23:46:54 +0700

    chore: add type into cryptocurrencies data

--- src/data/cryptocurrencies.ts
+++ src/data/cryptocurrencies.ts
@@ -1,22 +1,33 @@
-export const cryptocurrencies = [
+type Cryptocurrency = {
+  id: number;
+  name?: string | null;
+  type?: "Layer 1" | "Layer 2" | "Stablecoin" | "Memecoin" | null;
+};
+
+export const cryptocurrencies: Cryptocurrency[] = [
   {
     id: 1,
     name: "Bitcoin",
+    type: "Layer 1",
   },
   {
     id: 2,
     name: "Etherium",
+    type: "Layer 1",
   },
   {
     id: 3,
     name: "Tether",
+    type: "Stablecoin",
   },
   {
     id: 4,
     name: "XRP",
+    type: "Layer 1",
   },
   {
     id: 5,
     name: "BNB",
+    type: "Layer 1",
   },
 ];
