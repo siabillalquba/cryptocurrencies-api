462a4d3fabe6814a71b2c1d582eadcbd6a3172ca
Sia Billal Quba <siabillal@gmail.com>
2025-06-22 22:56:26 +0700

    feat: get /cryptocurrencies data

---
+++ src/data/cryptocurrencies.ts
@@ -0,0 +1,14 @@
+export const cryptocurrencies = [
+  {
+    id: 1,
+    name: "bitcoin",
+  },
+  {
+    id: 2,
+    name: "etherium",
+  },
+  {
+    id: 3,
+    name: "binance",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono";
+import { cryptocurrencies } from "./data/cryptocurrencies";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.json({
-    message: "Hello",
+    message: "Cryptocurrencies API",
+    cryptocurrencies: "/cryptocurrencies",
   });
+});
+
+app.get("/cryptocurrencies", (c) => {
+  return c.json(cryptocurrencies);
 });
 
 export default app;
