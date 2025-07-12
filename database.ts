ba37be1155d93144375c913ca78916d892081dd2
Sia Billal Quba <siabillal@gmail.com>
2025-07-12 21:57:36 +0700

    feat: connect to pg database adapter

--- database.ts
+++ database.ts
@@ -4,4 +4,28 @@ const client = new pg.Client({
   connectionString: process.env.DATABASE_URL,
 });
 
-await client.connect();
+async function getCryptocurrencies() {
+  await client.connect();
+
+  const result = await client.query("SELECT * FROM cryptocurrencies");
+  const cryptocurrencies = result.rows;
+  console.log({ cryptocurrencies });
+
+  await client.end();
+}
+
+async function addCryptocurrency() {
+  await client.connect();
+
+  const result = await client.query(
+    "INSERT INTO cryptocurrencies (name, symbol, founder) VALUES ('TOKENOMY', 'TEN', 'Oscar Darmawan');"
+  );
+  console.log({ result });
+
+  const cryptocurrencies = result.rows;
+  console.log({ cryptocurrencies });
+
+  await client.end();
+}
+
+getCryptocurrencies();
